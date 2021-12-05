69006f40ca70ba008b737106f8103177e9458b23
Erik Ecoologic <erikecoologic@gmail.com>
2021-12-05 17:25:55 +1000

    WIP

--- another_test.go
+++ another_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestCalculate(t *testing.T) {
-	if Calculate(2) != 4 {
+	if Calculate(2) != 5 {
 		t.Error("Expected 2 + 2 to equal 4")
 	}
 
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module go-chess
+module first
 
 go 1.17
