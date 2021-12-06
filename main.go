ba1a1f68b17d8f2fddc172e1d86e1b61f416a101
Erik Ecoologic <erikecoologic@gmail.com>
2021-12-06 16:44:06 +1000

    type piece rune

--- main.go
+++ main.go
@@ -21,47 +21,53 @@ import (
 
 func MakeBoard() BoardType {
 	board := boardMatrixType{
-		{"r", "n", "b", "q", "k", "b", "n", "r"}, // [0][0] (1A) White
-		{"p", "p", "p", "p", "p", "p", "p", "p"},
-		{"_", "_", "_", "_", "_", "_", "_", "_"},
-		{"_", "_", "_", "_", "_", "_", "_", "_"},
-		{"_", "_", "_", "_", "_", "_", "_", "_"}, // Assigned inverted
-		{"_", "_", "_", "_", "_", "_", "_", "_"},
-		{"P", "P", "P", "P", "P", "P", "P", "P"},
-		{"R", "N", "B", "Q", "K", "B", "N", "R"}, // [7][7] (8H) Black
+		{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}, // [0][0] (1A) White
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{'_', '_', '_', '_', '_', '_', '_', '_'},
+		{'_', '_', '_', '_', '_', '_', '_', '_'},
+		{'_', '_', '_', '_', '_', '_', '_', '_'}, // Assigned inverted
+		{'_', '_', '_', '_', '_', '_', '_', '_'},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}, // [7][7] (8H) Black
 	}
 	return BoardType{board}
 }
 
-type boardMatrixType [8][8]string
+//////////
 
 type BoardType struct {
 	boardMatrix boardMatrixType
 }
 
-func (b BoardType) At(position string) string {
+func (b BoardType) At(position positionType) pieceType {
 	return b.boardMatrix[matrixRowIndex(position)][matrixColumnIndex(position)]
 }
+
+//////////
+
+type positionType = string
+type pieceType rune
+type boardMatrixType [8][8]pieceType
 
 // board, position, coord, index
 // file, letter, y, column
 // rank, number, x, row
-func matrixColumnIndex(position string) (result int) {
-	result = int(strings.ToUpper(position)[0]) - int("A"[0])
+func matrixColumnIndex(position positionType) (result int) {
+	result = int(strings.ToUpper(position)[0]) - int('A')
 	return // naked return
 }
 
-func matrixRowIndex(position string) int {
+func matrixRowIndex(position positionType) int {
 	rank1to8, _ := strconv.Atoi(string(position[1]))
 	return rank1to8 - 1
 }
 
-func boardAt(board boardMatrixType, position string) string {
+func boardAt(board boardMatrixType, position positionType) pieceType {
 	return board[matrixRowIndex(position)][matrixColumnIndex(position)]
 }
 
 func main() {
 	board := MakeBoard()
 	fmt.Println("The board is: ", board)
-	fmt.Println("A1 is: ", board.At("A1"))
+	fmt.Println("D8 is: ", string(board.At("D8")))
 }
--- main_test.go
+++ main_test.go
@@ -11,14 +11,14 @@ func TestNewBoardMatrix(t *testing.T) {
 	D1 := board.boardMatrix[0][3]
 	E5 := board.boardMatrix[4][4]
 
-	if A8 != "R" {
-		t.Error(fmt.Sprintf("board should start with a black rook in A8, but was %s", A8))
+	if A8 != 'R' {
+		t.Error(fmt.Sprintf("board should start with a black rook in A8, but was %c", A8))
 	}
-	if D1 != "q" {
-		t.Error(fmt.Sprintf("board should start with a white queen in D1, but was %s", D1))
+	if D1 != 'q' {
+		t.Error(fmt.Sprintf("board should start with a white queen in D1, but was %c", D1))
 	}
-	if E5 != "_" {
-		t.Error(fmt.Sprintf("board should start nothing in E5, but was %s", E5))
+	if E5 != '_' {
+		t.Error(fmt.Sprintf("board should start nothing in E5, but was %c", E5))
 	}
 }
 
@@ -26,18 +26,18 @@ func TestBoardAt(t *testing.T) {
 	board := MakeBoard()
 	piece1 := boardAt(board.boardMatrix, "A1")
 
-	if piece1 != "r" {
-		t.Error(fmt.Sprintf("board at A1 should be a white rook, but was %s", piece1))
+	if piece1 != 'r' {
+		t.Error(fmt.Sprintf("board at A1 should be a white rook, but was %c", piece1))
 	}
 
 	piece2 := boardAt(board.boardMatrix, "D8")
-	if piece2 != "Q" {
-		t.Error(fmt.Sprintf("board at D8 should be a black queen, but was %s", piece2))
+	if piece2 != 'Q' {
+		t.Error(fmt.Sprintf("board at D8 should be a black queen, but was %c", piece2))
 	}
 
 	piece3 := boardAt(board.boardMatrix, "G1")
-	if piece3 != "n" {
-		t.Error(fmt.Sprintf("board at G1 should be a white knight, but was %s", piece3))
+	if piece3 != 'n' {
+		t.Error(fmt.Sprintf("board at G1 should be a white knight, but was %c", piece3))
 	}
 }
 
@@ -45,11 +45,11 @@ func TestBoardTypeAt(t *testing.T) {
 	board := MakeBoard()
 
 	piece1 := boardAt(board.boardMatrix, "B8")
-	if piece1 != "N" {
-		t.Error(fmt.Sprintf("board at B8 should be a black knight, but was %s", piece1))
+	if piece1 != 'N' {
+		t.Error(fmt.Sprintf("board at B8 should be a black knight, but was %c", piece1))
 	}
 	piece2 := boardAt(board.boardMatrix, "F2")
-	if piece2 != "p" {
-		t.Error(fmt.Sprintf("board at F2 should be a white pawn, but was %s", piece2))
+	if piece2 != 'p' {
+		t.Error(fmt.Sprintf("board at F2 should be a white pawn, but was %c", piece2))
 	}
 }
